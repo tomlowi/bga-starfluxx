3f5e051fcb5e65d557fe85a8e49bfefaf2406921
tomlowi <iwan.tomlow@gmail.com>
2021-10-17 20:23:27 +0200

    #50872: SonicSledgehammer issue due to bugfix #50750

--- modules/php/Cards/Actions/ActionSonicSledgehammer.php
+++ modules/php/Cards/Actions/ActionSonicSledgehammer.php
@@ -84,8 +84,12 @@ class ActionSonicSledgehammer extends ActionCard
         )
       );
 
-      if ($playersKeepersInPlay > 0 && count($player_selected_cards[$from_player_id]) != 1
-          || $player_selected_cards[$from_player_id][0]["type"] != "keeper") {
+      if ($playersKeepersInPlay > 0 && 
+          (count($player_selected_cards[$from_player_id]) != 1
+          || (count($player_selected_cards[$from_player_id]) == 1 
+              && $player_selected_cards[$from_player_id][0]["type"] != "keeper")
+          )
+        ) {
         Utils::throwInvalidUserAction(
           starfluxx::totranslate(
             "You must select exactly 1 Keeper card from each player"
